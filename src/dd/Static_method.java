9067e8f2ccea1950a0aa44920c9707ace79c3272
김승혜 <67942699+kshksh723@users.noreply.github.com>
2024-02-04 21:58:54 +0900

    2/4

---
+++ src/dd/Static_method.java
@@ -0,0 +1,12 @@
+package dd;
+
+import MyUtil.MyUtil;
+
+public class Static_method {
+
+	public static void main(String[] args) {
+		
+		MyUtil.current_time_print();
+	}
+
+}
---
+++ src/dd/Static_method2.java
@@ -0,0 +1,13 @@
+package dd;
+
+import MyUtil.MyUtil;
+
+public class Static_method2 {
+
+	public static void main(String[] args) {
+		
+		MyUtil util = new MyUtil();
+		util.current_time_print();
+	}
+
+}
